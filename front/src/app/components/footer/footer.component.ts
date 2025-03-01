8fc0ad440f119726cee0cd25820c56d966e45e86
adolfo_carrillo <gustavoadolfocarrillocamacho@gmail.com>
2025-03-01 14:00:54 -0600

    Fixed some alert related to lack of classes on the .scss general files.

--- front/src/app/components/footer/footer.component.ts
+++ front/src/app/components/footer/footer.component.ts
@@ -3,9 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule,
-    FooterComponent
-  ],
+  imports: [RouterModule],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
